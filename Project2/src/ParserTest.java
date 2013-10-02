03122083b87607a7b3ebc5d423855c6a5313d575
Marty <marsedge95@gmail.com>
2013-10-02 13:03:21 -0500

    Added test for Publication.getAuthorString()

--- Project2/src/ParserTest.java
+++ Project2/src/ParserTest.java
@@ -19,11 +19,20 @@ public class ParserTest {
 	}
 	
 	@Test
-	public void testParseFileAuthor() throws IOException {
+	public void testParseFileString() throws IOException {
 		Parser parser = new Parser("TestCase.txt");
 		
 		parser.parseFile();
 		
 		Assert.assertEquals(parser.getPublications().get(1).toString(), "Journal Article, Authors = Eskridge, Brent E.; Hougen, Dean F.; Paper Title = Extending Adaptive Fuzzy Behavior Hierarchies to Multiple Levels of Composite Behaviors, Serial Title = Robotics and Autonomous Systems, Starting Page = 1076, Ending Page = 1084, Time of Publication = September2010, Hyperlink = http://dx.doi.org/10.7551/978-0-262-31050-5-ch020");
+	}
+	
+	@Test
+	public void testParseFileAuthor() throws IOException {
+		Parser parser = new Parser("TestCase.txt");
+		
+		parser.parseFile();
+		
+		Assert.assertEquals(parser.getPublications().get(1).getAuthorsString().contains("Dean"), true);
 	}
 }
