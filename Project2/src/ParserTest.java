7f6c7ba4e188bf21ae12d431fee8e140b70b224b
Marty <marsedge95@gmail.com>
2013-10-02 08:14:03 -0500

    Add another test to Parser

--- Project2/src/ParserTest.java
+++ Project2/src/ParserTest.java
@@ -17,4 +17,13 @@ public class ParserTest {
 		
 		Assert.assertEquals(parser.getPublications().get(0).getTitlePaper(), "Sexual Selection, Resource Distribution, and Population Size in Synthetic Sympatric Speciation");
 	}
+	
+	@Test
+	public void testParseFileAuthor() throws IOException {
+		Parser parser = new Parser("TestCase.txt");
+		
+		parser.parseFile();
+		
+		Assert.assertEquals(parser.getPublications().get(1).toString(), "Journal Article, Authors = Eskridge, Brent E.; Hougen, Dean F.; Paper Title = Extending Adaptive Fuzzy Behavior Hierarchies to Multiple Levels of Composite Behaviors, Serial Title = Robotics and Autonomous Systems, Starting Page = 1076, Ending Page = 1084, Time of Publication = September2010, Hyperlink = http://dx.doi.org/10.7551/978-0-262-31050-5-ch020");
+	}
 }
