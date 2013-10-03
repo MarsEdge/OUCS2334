57bb482f46abf72f8b9873e3534f10daff2832c6
Marty <marsedge95@gmail.com>
2013-10-03 14:12:05 -0500

    Added partial stub Author class

--- Project3/src/Author.java
+++ Project3/src/Author.java
@@ -1,3 +1,33 @@
-public class Author {
+import java.util.ArrayList;
 
+public class Author {
+	
+	/**
+	 * name of the author
+	 */
+	private String name;
+	
+	/**
+	 * publications published by this author
+	 */
+	private ArrayList<String> publishedPapers = new ArrayList<String>();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public ArrayList<String> getPublishedPapers()
+	{
+		return publishedPapers;
+	}
+	
+	public boolean addPublishedPaper(String paper)
+	{
+		publishedPapers.add(paper);
+		return true;
+	}
 }
