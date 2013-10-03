3c7ba089b74acefb00a556958d03e11d0c277c46
Marty <marsedge95@gmail.com>
2013-10-02 20:13:15 -0500

    Made Driver use input panes, added some indentations

--- Project2/src/Driver.java
+++ Project2/src/Driver.java
@@ -24,87 +24,92 @@ public class Driver {
 		//For loop for repeating search
 		for(int i=0; i==0;){
 		
-		//Ask for and read in the file
-		JOptionPane.showMessageDialog(null, "Welcome to the Publication Searcher. Please click OK to continue in the console. \n\nPlease enter the name of the file. \n\n");
-		String filename = in.nextLine();
-		Parser p = new Parser(filename);		
+			//Ask for and read in the file
+			String filename = JOptionPane.showInputDialog ( "Please enter the name of the file" );
+			Parser p = new Parser(filename);
+			
+			//For loop for repeating search
+			for(int j=0; j==0;) {
+			
+				//Save input as the choice
+				
+				String choice = JOptionPane.showInputDialog ( "Choose a criteria to serach by entering the coressponding digit: \n\n1. Name of Author \n2. Name of Paper/Article \n3. Name of the Journal/Collection of Paper \n4. Exit the Program" );
+				int num = Integer.valueOf(choice);
 		
-		//For loop for repeating search
-		for(int j=0; j==0;){
-		
-		//Text-based user interface for searching the magazines
-		JOptionPane.showMessageDialog(null, "Choose a criteria to serach by entering the coressponding digit: \n\n1. Name of Author \n2. Name of Paper/Article \n3. Name of the Journal/Collection of Paper \n4. Exit the Program \n\n");
-
-		//Save input as the choice
-		String choice = in.nextLine();
-		int num = Integer.valueOf(choice);
-
-		//Manages choice using if statements
-		
-		if(num==1){JOptionPane.showMessageDialog(null, "You are searching by Name of Author. Please enter a name to search for.");}
-		else if(num==2){JOptionPane.showMessageDialog(null, "You are searching by Name of Article/Paper. Please enter a name to search for.");}
-		else if(num==3){JOptionPane.showMessageDialog(null, "You are searching by Name of Journal/Collection of Papers. Please enter a name to search for.");}
-		else if(num==4){i=1; System.exit(-1);}
-		else JOptionPane.showMessageDialog(null, "Please enter a valid option number.");
-		
-		//Search for data here using specified criteria type
-		String search = in.nextLine();
-		String results = "";
-		//the results of the search
-		Boolean resultsBool = false;
-		
-		//ArrayList of Publications made from Parser
-		ArrayList<Publication> pubList = new ArrayList<Publication>();
-		pubList = p.getPublications();
-		
-		//Search through magazines. Check that all magazines are stored into one large arraylist
-		if(num==1){
-			for(Publication pub : pubList){
-				if(pub.getAuthorsString() != null && pub.getAuthorsString().contains(search)){results += pub.toString() + "\n" ;resultsBool = true;}
-				else {resultsBool = false;}
-		}}
-		else if(num==2){
-			for(Publication pub : pubList){
-				if(pub.getTitlePaper() != null && pub.getTitlePaper().contains(search)){results += pub.toString() + "\n";resultsBool = true;}
-				else {resultsBool = false;}
-		}}
-		else if(num==3){
-			for(Publication pub : pubList){
-				if(pub.getTitleSerial() != null && pub.getTitleSerial().contains(search)){results += pub.toString() + "\n";resultsBool = true;}
-				else {resultsBool = false;}
-		}}
-		
-		//JOptionPane that displays search results
-		if(resultsBool==true) JOptionPane.showMessageDialog(null, results);
-		else if(resultsBool==false) JOptionPane.showMessageDialog(null, "There were no publications with that matched your search.");
-		
-		//Yes and No JOptionPane used to continue searching or quit program
-		if (JOptionPane.showConfirmDialog(null, "Do you wish to search again?", "WARNING", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-		    //Yes option
-			j=0;
-		} else {
-			//No option
-			j=1;
-			in.close();
-			System.exit(-1);
-		}
-		
-		}
-	
-		
-		//Yes and No JOptionPane used to search using a different file or quit program
-		if (JOptionPane.showConfirmDialog(null, "Do you wish to search a different file?", "WARNING", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-		    //Yes option
-			i=0;
-		} else {
-			//No option
-			i=1;
-			in.close();
-			System.exit(-1);
-		}
-		
+				//Manages choice using if statements
+				
+				String search = "";
+				if(num==1){ search = JOptionPane.showInputDialog ( "You are searching by Name of Author. Please enter a name to search for" );}
+				else if(num==2){search = JOptionPane.showInputDialog ( "You are searching by Name of Article/Paper. Please enter a name to search for");}
+				else if(num==3){search = JOptionPane.showInputDialog ( "You are searching by Name of Journal/Collection of Papers. Please enter a name to search for");}
+				else if(num==4){i=1; System.exit(-1);}
+				else JOptionPane.showMessageDialog(null, "Please enter a valid option number.");
+				
+				//Search for data here using specified criteria type
+				String results = "";
+				//the results of the search
+				Boolean resultsBool = false;
+				
+				//ArrayList of Publications made from Parser
+				ArrayList<Publication> pubList = new ArrayList<Publication>();
+				pubList = p.getPublications();
+				
+				//Search through magazines. Check that all magazines are stored into one large arraylist
+				if(num==1)
+				{
+					for(Publication pub : pubList)
+					{
+						if(pub.getAuthorsString() != null && pub.getAuthorsString().contains(search)){results += pub.toString() + "\n" ;resultsBool = true;}
+						else {resultsBool = false;}
+					}
+				}
+				else if(num==2)
+				{
+					for(Publication pub : pubList)
+					{
+						if(pub.getTitlePaper() != null && pub.getTitlePaper().contains(search)){results += pub.toString() + "\n";resultsBool = true;}
+						else {resultsBool = false;}
+					}
+				}
+				else if(num==3)
+				{
+					for(Publication pub : pubList)
+					{
+						if(pub.getTitleSerial() != null && pub.getTitleSerial().contains(search)){results += pub.toString() + "\n";resultsBool = true;}
+						else {resultsBool = false;}
+					}
+				}
+				
+				//JOptionPane that displays search results
+				if(resultsBool==true) JOptionPane.showMessageDialog(null, results);
+				else if(resultsBool==false) JOptionPane.showMessageDialog(null, "There were no publications with that matched your search.");
+				
+				//Yes and No JOptionPane used to continue searching or quit program
+				if (JOptionPane.showConfirmDialog(null, "Do you wish to search again?", "WARNING", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+				    //Yes option
+					j=0;
+				} else 
+				{
+					//No option
+					j=1;
+					in.close();
+					System.exit(-1);
+				}
+			
+			}
+			
+			
+			//Yes and No JOptionPane used to search using a different file or quit program
+			if (JOptionPane.showConfirmDialog(null, "Do you wish to search a different file?", "WARNING", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+			    //Yes option
+				i=0;
+			} else 
+			{
+				//No option
+				i=1;
+				in.close();
+				System.exit(-1);
+			}
 		}
 	}
-
-
 }
