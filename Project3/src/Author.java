b538b4cd16b8c23ac2959ef7b32f0b57b88f0dc9
Marty <marsedge95@gmail.com>
2013-10-07 11:58:18 -0500

    divide Author name into first and last

--- Project3/src/Author.java
+++ Project3/src/Author.java
@@ -3,21 +3,27 @@ import java.util.ArrayList;
 public class Author {
 	
 	/**
-	 * name of the author
+	 * first name of the author
 	 */
-	private String name;
+	private String nameFirst;
+	
+	/**
+	 * last name of the author
+	 */
+	private String nameLast;
 	
 	/**
 	 * publications published by this author
 	 */
 	private ArrayList<String> publishedPapers = new ArrayList<String>();
 	
-	public String getName() {
-		return name;
+	public String toString() {
+		return getNameFirst() + " " + getNameLast();
 	}
 	
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String nameWhole) {
+		setNameLast(nameWhole.split("\\, ")[0]);
+		setNameFirst(nameWhole.split("\\, ")[1]);
 	}
 	
 	public ArrayList<String> getPublishedPapers()
@@ -29,5 +35,21 @@ public class Author {
 	{
 		publishedPapers.add(paper);
 		return true;
+	}
+
+	public String getNameFirst() {
+		return nameFirst;
+	}
+
+	public void setNameFirst(String nameFirst) {
+		this.nameFirst = nameFirst;
+	}
+
+	public String getNameLast() {
+		return nameLast;
+	}
+
+	public void setNameLast(String nameLast) {
+		this.nameLast = nameLast;
 	}
 }
