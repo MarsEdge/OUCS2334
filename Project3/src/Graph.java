e7b2fdf49c40639274aae6d07319093e5f3be7d1
John Frashier <loplopol@gmail.com>
2013-10-08 20:39:37 -0500

    Added Graph class

---
+++ Project3/src/Graph.java
@@ -0,0 +1,52 @@
+import javax.swing.JFrame;
+
+
+public class Graph extends JFrame{
+
+	
+	public String type;
+	public String xLabel;
+	public String yLabel;
+	public String title;
+
+	/**
+	 * Default Graph shown at start of program
+	 */
+	public Graph(){
+		this.type = "";
+		this.xLabel = "";
+		this.yLabel = "";
+		this.title = "";
+	}
+	
+	/**
+	 * Constructor for a Graph given the type of graph asked for by the user
+	 * @param type        type of graph to create
+	 */
+	public Graph(String type){
+		
+		this.type = type;
+		typeSetVariables(type);
+		
+		
+	}
+	
+	/**
+	 * Sets the variables used to create each graph given the type of graph
+	 * @param type		type of graph
+	 */
+	public static void typeSetVariables(String type){
+		//implement which x and y labels and title goes for each type
+	}
+	
+	
+	/**
+	 * Displays the desired graph in a GUI.
+	 * @param graph			Graph object which contains information for graph
+	 */
+	public static void displayGraph(Graph graph) {
+		//gui creation here
+	}
+	
+	
+}
