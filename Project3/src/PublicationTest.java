0c532ddbc719e724fcf82b9dfe134762e5824e84
Marty <marsedge95@gmail.com>
2013-10-23 13:49:32 -0500

    Added Publication unit tests

---
+++ Project3/src/PublicationTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class PublicationTest {
+
+	@Test
+	public void testConstructor() {		
+		ArrayList<String> authors = new ArrayList<String>();
+		
+		authors.add("FName LName");
+		
+		Publication pub = new Publication(authors, "titlePaper", "titleSerial", "pageStart", "pageEnd", "month", "year", "link");
+		
+		Assert.assertEquals(pub.getTitlePaper(), "titlePaper");
+		
+		Assert.assertEquals(pub.getTitleSerial(), "titleSerial");
+		
+		Assert.assertEquals(pub.getAuthorsString(), "FName LName; ");
+		
+		Assert.assertEquals(pub.getPageStart(), "pageStart");
+		
+		Assert.assertEquals(pub.getPageEnd(), "pageEnd");
+		
+		Assert.assertEquals(pub.getPageEnd(), "pageEnd");
+		
+		Assert.assertEquals(pub.getDate(), "month year");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAuthors() {
+		fail("Not yet implemented");
+	}
+
+}
