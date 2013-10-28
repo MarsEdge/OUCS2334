8e0e4fc127dd270c2ac026b7ca2a2f367df9cbeb
Marty Rand <marsedge95@gmail.com>
2013-10-28 12:51:43 -0600

    Started adding new classes to Project 4

---
+++ Project4/src/Conference.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Conference {
+	
+	private Name sponsor;
+	
+	private ArrayList<Meeting> meetings;
+
+	public Name getSponsor() {
+		return sponsor;
+	}
+
+	public void setSponsor(Name sponsor) {
+		this.sponsor = sponsor;
+	}
+	
+}
