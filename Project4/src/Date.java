7bc53bb7f978d832901c1474cd9070aaf67355e4
Marty Rand <marsedge95@gmail.com>
2013-10-29 07:27:54 -0600

    Added constructors to Date

--- Project4/src/Date.java
+++ Project4/src/Date.java
@@ -3,7 +3,17 @@ public class Date {
 	private String month;
 	
 	private String year;
-
+	
+	public Date() {
+		setMonth("No Month Entered");
+		setYear("No Year Entered");
+	}
+	
+	public Date(String month, String year) {
+		setMonth(month);
+		setYear(year);
+	}
+	
 	public String getMonth() {
 		return month;
 	}
