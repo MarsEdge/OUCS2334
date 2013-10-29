2918ae0086c0b2e56b4d232facb8e89c532fae46
Marty Rand <marsedge95@gmail.com>
2013-10-29 10:37:40 -0600

    Added getters / setters to Journal

--- Project4/src/Journal.java
+++ Project4/src/Journal.java
@@ -10,5 +10,37 @@ public class Journal {
 	
 	/** list of volumes in Journal */
 	private ArrayList<Volume> volumes;
+
+	public Name getPubName() {
+		return pubName;
+	}
+
+	public void setPubName(Name pubName) {
+		this.pubName = pubName;
+	}
+
+	public Location getPubLocation() {
+		return pubLocation;
+	}
+
+	public void setPubLocation(Location pubLocation) {
+		this.pubLocation = pubLocation;
+	}
+
+	public ArrayList<Volume> getVolumes() {
+		return volumes;
+	}
+
+	public void setVolumes(ArrayList<Volume> volumes) {
+		this.volumes = volumes;
+	}
+	
+	public void addVolume(Volume volume) {
+		this.volumes.add(volume);
+	}
+	
+	public void removeResearchArea(Volume volume) {
+		this.volumes.remove(volume);
+	}
 	
 }
