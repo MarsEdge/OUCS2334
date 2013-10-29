d00e06b4a443afc3b634ba0f24843262c9acca7e
Marty Rand <marsedge95@gmail.com>
2013-10-29 10:35:26 -0600

    Added JavaDoc to Journal

--- Project4/src/Journal.java
+++ Project4/src/Journal.java
@@ -2,10 +2,13 @@ import java.util.ArrayList;
 
 public class Journal {
 	
+	/** publisher name */
 	private Name pubName;
 	
+	/** publisher location */
 	private Location pubLocation;
 	
+	/** list of volumes in Journal */
 	private ArrayList<Volume> volumes;
 	
 }
