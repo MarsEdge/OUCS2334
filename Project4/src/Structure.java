d597c960239b6ffe2706b7052a123e30d17135d9
Marty Rand <marsedge95@gmail.com>
2013-11-13 11:16:55 -0600

    fix main() ordering

--- Project4/src/Structure.java
+++ Project4/src/Structure.java
@@ -2,13 +2,12 @@ public class Structure {
 	
 	private static ScholarshipModel model;
 	private static SelectionView view = new SelectionView();
-	private ScholarPubController controller;
+	private static ScholarPubController controller;
 	
 	public static void main(String args[]) {
-		 ScholarPubController controller = new ScholarPubController(model, view);
 		model = new ScholarshipModel();
 		view.setModel(model);
-		view.setModel(model);
+		controller = new ScholarPubController(model, view);
 	}
 
 }
